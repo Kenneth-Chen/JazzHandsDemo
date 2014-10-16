9ee2e6a1d6b1b797931caac4738ec6e9abbde935
Kenneth Chen <kennchen@gmail.com>
2014-10-16 11:12:34 -0400

    init commit

---
+++ Assets/Qualcomm Augmented Reality/Scripts/FaceLaser.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceLaser : MonoBehaviour {
+	
+	public GameObject gunBarrel;
+	public GameObject cameraObject;
+	private double doorDelayTime = 3.0;
+	private double nextFireTime = 0.0;
+	private GameObject target;
+	//private Texture2D door_green = Resources.Load("door_green_bg") as Texture2D;
+	//private Texture2D door_trans = Resources.Load("door_trans_bg") as Texture2D;
+	
+	//Use this for initilization
+	void Start ()
+	{
+		
+	}
+	
+	//Update is called once per frame
+	void Update ()
+	{
+		Fire();
+	}
+	
+	void jumpLocation()
+	{
+		Debug.Log ("Weapon.jumpLocation: " + target.name);
+		teleport ();
+		resetDoorway ();
+	}
+	void teleport()
+	{
+		Debug.Log ("Weapon.teleport()");
+//		Portal doorway_portal = (Portal) target.GetComponent(typeof(Portal));
+//		doorway_portal.teleport();
+	}
+	void set_visibility(bool vis)
+	{
+		Color targetColor = target.renderer.material.color;
+		if (vis == true) 
+		{
+			//target.renderer.material.mainTexture=door_green;
+			//target.renderer.material.color = new Color (targetColor.r, targetColor.g, targetColor.b, (float)0.5);
+			target.renderer.material.color = new Color (0, 120, 11, (float)0.75);
+		} else {
+			//target.renderer.material.mainTexture=door_trans;
+			target.renderer.material.color =new Color(255,255,255, (float)0.0);
+		}
+	}
+	void resetDoorway()
+	{
+		if (target != null) 
+		{
+			//print("resetDoorway : "+doorway.name);
+			set_visibility(false);
+			nextFireTime = 0.0;
+		}
+	}
+	
+	void Fire()
+	{
+		//add reload delay for fire - no button mashing : )
+		
+		RaycastHit hit;
+		
+		if (Physics.Raycast(gunBarrel.transform.position, gunBarrel.transform.forward, out hit))
+		{
+			
+			if(hit.collider.tag == "Doorway")
+			{
+				target = hit.collider.gameObject;
+				
+				//doorway.renderer.material.color = new Color( 0, 0, 255, 0 );
+				
+				
+				//if not set, show the box and start timer
+				if(nextFireTime == 0.0)
+				{
+					//print("Hit : "+doorway.name);
+					nextFireTime = Time.time + doorDelayTime; 
+					float alphaDelta = (float)(Time.deltaTime/doorDelayTime);
+					set_visibility(true);
+				}
+				//if it's been long enough, go through the door
+				if(Time.time > nextFireTime)
+				{
+					//cameraObject.transform.position = new Vector3(5, 0, 0);
+					jumpLocation();
+				}
+			}else{
+				resetDoorway();}
+			
+		}else{
+			//if not hitting the doorway, hide the doorway and reset timer
+			resetDoorway();
+			
+		}
+	}
+}
---
+++ Assets/TargetPositioning.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetPositioning : MonoBehaviour
+{
+	
+	private bool state1 = false;
+	private bool state2 = false;
+	
+	
+	private Vector3 initialSF;
+	private Vector3 initialPos;
+	public GameObject model;
+	
+	private float counter = 4;
+	
+	
+	void Start ()
+	{
+		initialSF = this.transform.localScale;
+		initialPos = this.model.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (DefaultTrackableEventHandler.TRACKING) {
+			float f = -this.transform.position.y;
+			float test = (2 / (this.initialSF.x * f));
+			float scalingFactor = 2.0f;
+			float positionFactor = 1.0f;
+			if (f != 0f && this.initialSF.x != 0f && !float.IsInfinity(test)) {
+				this.model.transform.localScale = new Vector3 (scalingFactor * counter / (this.initialSF.x * f), scalingFactor * counter / (this.initialSF.y * f), scalingFactor * counter / (this.initialSF.z * f));
+				this.model.transform.localPosition = new Vector3(positionFactor*(this.transform.position.x-this.initialPos.x)/(f*f), positionFactor*this.initialPos.y, positionFactor*(this.transform.position.z-this.initialPos.z)/(f*f));
+				//Debug.Log(f);
+			}
+			state1 = true;
+		} else if (state1) {
+			state1 = false;
+		}
+		if (TrackableEventHandler.TRACKING) {
+			/*if(!state2){
+							psOn.Play();
+						}*/
+			if (this.transform.position.y > 100) {
+				Vector3 v = new Vector3 (0, -200, 0);
+				this.transform.position += v;
+			}
+			state2 = true;
+		} else if (state2) {
+			state2 = false;
+		}
+	}
+	
+	private float symLog(float value){
+		if (value < 0) {
+			return (-Mathf.Log((-value)+1,10));
+		} else if (value > 0) {
+			return (Mathf.Log (value+1,10));
+		} else {
+			return 0f;
+		}
+	}
+	
+}
